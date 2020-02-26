1bca560af3617153cab79ac534f239a03d92652f
Zhenya Rusak <unsgmasters@gmail.com>
2020-02-26 23:10:15 +0300

    feat: implement common approach with different consumers

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 
 exports.min = function min (array) {
-  return 0;
+    return common(array, (item, index) => {
+        if (index == 0) {
+            this.result = item;
+        }
+        if (item < this.result) {
+            this.result = item;
+        }
+    });
 }
 
 exports.max = function max (array) {
-  return 0;
+    return common(array, (item, index) => {
+        if (index == 0) {
+            this.result = item;
+        }
+        if (item > this.result) {
+            this.result = item;
+        }
+    });
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    return common(array, (item, index) => {
+        if (index == 0) {
+            this.sum = 0;
+        }
+        this.sum += item;
+        if (index == array.length - 1) {
+            this.result = sum / array.length;
+        }
+    });
+}
+
+function common(array, consumer) {
+    if (array == undefined || array.length == 0) {
+        return 0;
+    }
+    array.forEach((item, index) => consumer(item, index));
+    return result;
 }
